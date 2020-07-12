916037954d96b54df9321327c4e0470e24432294
Prateek Gupta <prateekjpg@gmail.com>
2020-07-12 23:19:05 +0530

    Add day 05 code files

---
+++ 05-Flex Panel Gallery/index.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+    console.log('Hello');
+    this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+    console.log(e.propertyName);
+    if (e.propertyName.includes('flex')) { // this is because safari takes flex while other take flex-grow property
+        this.classList.toggle('open-active');
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
